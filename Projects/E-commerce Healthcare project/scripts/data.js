6973b13b1d128c59315db1e39768925ad61e47f4
codebykuldeep <kuldeep.pal@celestialsys.com>
2024-10-09 18:12:30 +0530

    project js files update

---
+++ Projects/E-commerce Healthcare project/scripts/data.js
@@ -0,0 +1,42 @@
+const productData =[
+    {
+        productId: "1",
+        name: "Wireless Earbuds",
+        brand: "TechBrand",
+        price: 4999,
+        discount: 10, 
+        image: "glucometer.jpg"
+    },
+    {
+        productId: "2",
+        name: "Wireless Earbuds",
+        brand: "TechBrand",
+        price: 4999,
+        discount: 10, 
+        image: "steam-machine.jpg"
+    },
+    {
+        productId: "3",
+        name: "Wireless Earbuds",
+        brand: "TechBrand",
+        price: 4999,
+        discount: 10, 
+        image: "vitamin-k2.jpg"
+    },
+    {
+        productId: "4",
+        name: "Wireless Earbuds",
+        brand: "TechBrand",
+        price: 4999,
+        discount: 10,
+        image: "yellow.jpg"
+    },
+    {
+        productId: "4",
+        name: "Wireless Earbuds",
+        brand: "TechBrand",
+        price: 4999,
+        discount: 10,
+        image: "vitamin.jpg"
+    }
+];
