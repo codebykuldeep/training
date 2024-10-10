14fc1faf5bf983699c5a2604885f92d5ed1870a0
codebykuldeep <kuldeep.pal@celestialsys.com>
2024-10-10 18:26:14 +0530

    Day 12 update

--- javascript/Day 12/debouncing/script.js
+++ javascript/Day 12/debouncing/script.js
@@ -1,11 +1,18 @@
 const withoutDebounce =document.getElementById('without');
 const withDebounce =document.getElementById('debounce');
-
+const op1 =document.getElementById('op1');
+const op2 =document.getElementById('op2');
 
 let count =0;
 
 function getData(){
     console.log(withoutDebounce.value);
+    op1.innerHTML +=`<br>${withoutDebounce.value}`;
+}
+
+function getData2(){
+    console.log(withDebounce.value);
+    op2.innerHTML +=`<br>${withDebounce.value}`;
     
 }
 
@@ -21,5 +28,5 @@ const doDebouncing = (fn,delay)=>{
 
 
 
-const debounce = doDebouncing(getData,300)
+const debounce = doDebouncing(getData2,300)
 
