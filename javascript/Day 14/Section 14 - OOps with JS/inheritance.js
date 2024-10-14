d2801300ef15afcad8d61b5a1eea2e1c81c9fe8e
codebykuldeep <kuldeep.pal@celestialsys.com>
2024-10-14 18:21:25 +0530

    Day 14 update

--- javascript/Day 14/Section 14 - OOps with JS/inheritance.js
+++ javascript/Day 14/Section 14 - OOps with JS/inheritance.js
@@ -1,17 +1,48 @@
-const Person = function (name,year){
-    this.name = name;
-    this.year= year;
-}
+const Person = function (firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  };
+  
+  Person.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear);
+  };
+  
+  const Student = function (firstName, birthYear, course) {
+    Person.call(this, firstName, birthYear);
+    this.course = course;
+  };
+  
+  // Linking prototypes
+  Student.prototype = Object.create(Person.prototype);
+  
+  Student.prototype.introduce = function () {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+  };
+  
+  const mike = new Student('Mike', 2020, 'Computer Science');
+  mike.introduce();
+  mike.calcAge();
+  
+  console.log(mike.__proto__);
+  console.log(mike.__proto__.__proto__);
+  
+  console.log(mike instanceof Student);
+  console.log(mike instanceof Person);
+  console.log(mike instanceof Object);
+  
+  Student.prototype.constructor = Student;
+  console.dir(Student.prototype.constructor);
 
-const Student = function(name,year,course){
-    Person.call(this,name,year);
-    this.course= course;
-}
-
-const s1 = new Student('jonas',2000,'CSE');
-
-console.log(s1);
 
 
-//linking prototypes
-Student.prototype = Object.create(Person.prototype)
+  /*
+  My name is Mike and I study Computer Science
+    test.js:7 17
+    test.js:26 Person {introduce: ƒ}
+    test.js:27 {calcAge: ƒ}
+    test.js:29 true
+    test.js:30 true
+    test.js:31 true
+    test.js:34 ƒ Student(firstName, birthYear, course)
+  
+  */
