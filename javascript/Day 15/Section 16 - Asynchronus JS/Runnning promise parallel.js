bf0c62f99badfddaf72942efc200e2cddf9597a6
codebykuldeep <kuldeep.pal@celestialsys.com>
2024-10-15 18:18:43 +0530

    day 15 update

--- javascript/Day 15/Section 16 - Asynchronus JS/Runnning promise parallel.js
+++ javascript/Day 15/Section 16 - Asynchronus JS/Runnning promise parallel.js
@@ -18,18 +18,6 @@ const getDataWithError = async (i)=>{
     
  }
  
- //here we are using fetching data one after another but it is not necessary to do it like that
- const getName=async(a,b,c)=>{
-    const data1 = await getDataWithError(a);
-    const data2 = await getDataWithError(b);
-    const data3 = await getDataWithError(c);
-    
-    
-    console.log(data1,data2,data3);
-    
-    
- }
- getName(1,2,3);
 
 
  const fetchParallel =async(a,b,c)=>{
@@ -38,8 +26,8 @@ const getDataWithError = async (i)=>{
     console.log(arrData);
     
  }
- fetchParallel();
+ fetchParallel(1,2,3);
 
- 
+ //it will short circuit if one of promise failse
  
  
