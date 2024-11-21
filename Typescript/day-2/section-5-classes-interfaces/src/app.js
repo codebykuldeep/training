e1beb012da569c603b059d7901c951371a0c4cc8
codebykuldeep <kuldeep.pal@celestialsys.com>
2024-11-21 18:13:58 +0530

    Typescript Day 2

---
+++ Typescript/day-2/section-5-classes-interfaces/src/app.js
@@ -0,0 +1,8 @@
+var Department = /** @class */ (function () {
+    function Department(n) {
+        this.name = n;
+    }
+    return Department;
+}());
+var accounting = new Department('accounting');
+console.log(accounting);
